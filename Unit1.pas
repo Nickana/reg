7fdc4455e5e5393d0b4b4eba4c7e5e1e2795711d
Aleksandr <marshsnader@yandex.ru>
2023-06-13 13:57:41 +0300

    Реализованно подключение(решена проблема с devide by 0 из за чего не читались данные)

--- Unit1.pas
+++ Unit1.pas
@@ -25,6 +25,8 @@ type
     timer_smena: TTimer;
     timer_date: TTimer;
     timer_table: TTimer;
+    Button1: TButton;
+    Button2: TButton;
     procedure read_data_buttonClick(Sender: TObject);
     procedure Connect_buttonClick(Sender: TObject);
     procedure ComDataPacket1Packet(Sender: TObject; const Str: string);
@@ -35,6 +37,9 @@ type
     procedure timer_smenaTimer(Sender: TObject);
     procedure timer_dateTimer(Sender: TObject);
     procedure timer_tableTimer(Sender: TObject);
+    procedure resetTime_buttonClick(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -43,7 +48,7 @@ type
 
 var
   Form1: TForm1;
-  Data: array of array of Integer;
+  Data: array of array of Integer; //массив данных в который заносится обработанный пакет с весов.
   I : Integer; // для массива в который будет заноситься data
 
 implementation
@@ -65,11 +70,7 @@ begin
         End;
       Memo1.Lines.Add(Str);
     End;
-      
 
-      
-    
-      
 
 end;
 
@@ -85,6 +86,24 @@ end;
 
 
 
+procedure TForm1.resetTime_buttonClick(Sender: TObject);
+Var
+today : TDateTime;
+date : string;
+value : integer;
+begin
+  today := Now;
+  value := 4;
+//date := (DateToStr(today) + '\'+ TimeToStr(today));
+  date := '00-00-00\00:00:00\';
+//date := '09-15-23\12:33:48\';
+
+  ComPort.Write(value,1);
+
+  ComPort.WriteStr(date);
+  //Memo1.Lines.Add(date);
+
+end;
 procedure TForm1.set_date_buttonClick(Sender: TObject);
 Var
 today : TDateTime;
@@ -93,14 +112,14 @@ value : integer;
 begin
   today := Now;
   value := 4;
-//  date := (DateToStr(today) + '\'+ TimeToStr(today));
+//date := (DateToStr(today) + '\'+ TimeToStr(today));
   date := FormatDateTime('dd-mm-yy\hh:mm:ss',today) +'\';
-  //date := '09-15-23\12:33:48\';
+//date := '09-15-23\12:33:48\';
 
   ComPort.Write(value,1);
 
   ComPort.WriteStr(date);
-  Memo1.Lines.Add(date);
+  //Memo1.Lines.Add(date);
 
 end;
 
@@ -123,6 +142,34 @@ begin
   timer_table.Enabled := false;
   XML_button.Caption := 'Cгенерировать таблицу весов';
   XML_button.Font.Color := clBlack;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);// сброс времени даты в 0
+Var
+today : TDateTime;
+date : string;
+value : integer;
+begin
+  today := Now;
+  value := 4;
+//date := (DateToStr(today) + '\'+ TimeToStr(today));
+  date := '00-00-00\00:00:00\';
+//date := '09-15-23\12:33:48\';
+
+  ComPort.Write(value,1);
+
+  ComPort.WriteStr(date);
+  //Memo1.Lines.Add(date);
+
+end;
+
+procedure TForm1.Button2Click(Sender: TObject); //дебажная команда для выхода из цикла соединения без очистки памяти
+var
+Value : integer;
+begin
+  Value := 5;
+  ComPort.Write(Value,1);
+
 end;
 
 procedure TForm1.ClearMemory_buttonClick(Sender: TObject);
@@ -150,83 +197,192 @@ proc, z:  integer;//переменная для заполнения прогресс бара
 
 byteArray : array[0..1] of byte;
 begin
-//    if Str[2] = '*' then //символ успешности соединения
-//      begin
-//        connect_indicator.Color := clGreen;
-//        connect_indicator_2.Color := clGreen;
-//        Memo1.Lines.add(Str);
-//      end;
-//    if Str[2] = '&' then   //символ для даты
+
+    case Str[2] of
+    '@' : begin
+              recievedValue := Copy(Str, 3, Length(Str) - 3);// делаем - 3 потому что в str существует символы окончания str в виде \n;
+              cutNum := StrToInt(recievedValue);
+              SetLength(Data,cutNum,7);
+              Memo1.Lines.Add(IntToStr(Length(Data)));
+          end;
+
+    '+' : begin
+             hilowInt := 0;
+             recievedValue := Str;
+             recievedValue := StringReplace(recievedValue, '<', '', [rfReplaceAll]);
+             recievedValue := StringReplace(recievedValue, '>', '', [rfReplaceAll]);
+             recievedValue := StringReplace(recievedValue, #10, '', [rfReplaceAll]);
+
+             subStrings := SplitString(recievedValue, ' ');
+
+             byteArray[0] := Byte(StrToInt(subStrings[6]));  //старший
+             byteArray[1] := Byte(StrToInt(subStrings[7]));  //младший
+
+             hilowInt:= (byteArray[0] shl 8) or byteArray[1];//совмещаем старший и младший байт
+
+             SetLength(subStrings,7);//сокращаем массив на 1 элемент
+                                                                       //ОСТАНОВИЛСЯ ТУТ, СДЕЛАТЬ АДЕКВАТНОЕ СОЕДИНЕНИЕ
+             subStrings[6] := IntToStr(hilowInt);
+
+             recievedValue := '';
+
+             for J := 0 to 6 do
+              begin
+               Data[I,J] :=StrToInt(SubStrings[J]);
+               recievedValue := recievedValue + IntToStr(Integer(Data[I,J])) +' ';
+              end;
+
+             I := I+1;
+
+             proc:= ((I*100) div Length(Data));
+             read_progress_bar.Position :=proc;
+             if proc < 100 then
+               begin
+                 Connect_button.Enabled := false;
+                 read_data_button.Enabled := false;
+                 XML_button.Enabled := false;
+                 set_date_button.Enabled :=false;
+                 ClearMemory_button.Enabled :=false;
+               end
+               else
+               begin
+                 Connect_button.Enabled := true;
+                 read_data_button.Enabled := true;
+                 XML_button.Enabled := true;
+                 set_date_button.Enabled :=true;
+                 ClearMemory_button.Enabled :=true;
+                 read_progress_bar.Position := 0;
+               end
+          end;
+
+    '*' : begin
+            Memo1.Lines.add(Str);
+            connect_indicator.Color := clGreen;
+            connect_indicator_2.Color := clGreen;
+          end;
+
+    '&' : begin
+            Memo1.Lines.Add(Str + ' Время установлено');
+          end;
+
+    '!' : begin
+            Memo1.Lines.Add(Str + ' Завершение подключения С очисткой памяти выполнено');
+            connect_indicator.Color := clRed;
+            connect_indicator_2.Color := clRed;
+            ComPort.Close;
+          end;
+
+    '#' : begin
+            Memo1.Lines.Add(Str + ' Завершение подключения без очистки памяти выполнено');
+            connect_indicator.Color := clRed;
+            connect_indicator_2.Color := clRed;
+          end
+    end;
+
+// proc:= ((I*100) div Length(Data));
+// read_progress_bar.Position :=proc;
+// if proc < 100 then
+//   begin
+//     Connect_button.Enabled := false;
+//     read_data_button.Enabled := false;
+//     XML_button.Enabled := false;
+//     set_date_button.Enabled :=false;
+//     ClearMemory_button.Enabled :=false;
+//   end
+//   else
+//   begin
+//     Connect_button.Enabled := true;
+//     read_data_button.Enabled := true;
+//     XML_button.Enabled := true;
+//     set_date_button.Enabled :=true;
+//     ClearMemory_button.Enabled :=true;
+//   end
+end;
+
+
+
+//
+//    if Str[2] = '&' then   //Символ для даты
 //      begin
 //        timer_date.Enabled := true;
 //        set_date_button.Caption := 'Дата и время установлены';
 //        set_date_button.Font.Color := clGreen;
 //      end;
-//
+////
 //    if Str[2] = '%' then  //Символ для смены
 //      begin
 //        timer_smena.Enabled := true;
 //        ClearMemory_button.Caption := 'Смена завершена';
 //        ClearMemory_button.Font.Color := clGreen;
+//        Memo1.Lines.add(Str);
 //      end;
+//
 
-    if Str[2] = '@' then  //символ для количеств даных(длинна массива)
-      Begin
-      recievedValue := Copy(Str, 3, Length(Str) - 3);// делаем - 3 потому что в str существует символы окончания str в виде \n;
-      cutNum := StrToInt(recievedValue);
-      SetLength(Data,cutNum,7);
-      Memo1.Lines.Add(IntToStr(Length(Data)));
-      End
+//    else if Str[2] = '*' then //символ успешности соединения
+//     begin
+//       connect_indicator.Color := clGreen;
+//       connect_indicator_2.Color := clGreen;
+//       Memo1.Lines.add(Str);
+//     End
+//--осноыв(раскментить если с свич кейс не получится
+//    if Str[2] = '@' then  //символ для количеств даных(длинна массива)
+//      Begin
+//      recievedValue := Copy(Str, 3, Length(Str) - 3);// делаем - 3 потому что в str существует символы окончания str в виде \n;
+//      cutNum := StrToInt(recievedValue);
+//      SetLength(Data,cutNum,7);
+//      Memo1.Lines.Add(IntToStr(Length(Data)));
+//      End
+//
+//    else
+//    begin
+//        hilowInt := 0;
+//        recievedValue := Str;
+//        recievedValue := StringReplace(recievedValue, '<', '', [rfReplaceAll]);
+//        recievedValue := StringReplace(recievedValue, '>', '', [rfReplaceAll]);
+//        recievedValue := StringReplace(recievedValue, #10, '', [rfReplaceAll]);
+//
+//        subStrings := SplitString(recievedValue, ' ');
+//
+//        byteArray[0] := Byte(StrToInt(subStrings[6]));   //старший
+//        byteArray[1] := Byte(StrToInt(subStrings[7]));  //младший
+//
+//        hilowInt:= (byteArray[0] shl 8) or byteArray[1];//совмещаем старший и младший байт
+//
+//        SetLength(subStrings,7);//сокращаем массив на 1 элемент
+//                                                                  //ОСТАНОВИЛСЯ ТУТ, СДЕЛАТЬ АДЕКВАТНОЕ СОЕДИНЕНИЕ
+//        subStrings[6] := IntToStr(hilowInt);
+//
+//        recievedValue := '';
+//
+//      for J := 0 to 6 do
+//        begin
+//          Data[I,J] :=StrToInt(SubStrings[J]);
+//          recievedValue := recievedValue + IntToStr(Integer(Data[I,J])) +' ';
+//        end;;
+//
+//      I := I+1;
+//    end;
+//      proc:= ((I*100) div Length(Data));
+//      read_progress_bar.Position :=proc;
+//      if proc < 100 then
+//        begin
+//          Connect_button.Enabled := false;
+//          read_data_button.Enabled := false;
+//          XML_button.Enabled := false;
+//          set_date_button.Enabled :=false;
+//          ClearMemory_button.Enabled :=false;
+//        end
+//        else
+//        begin
+//          Connect_button.Enabled := true;
+//          read_data_button.Enabled := true;
+//          XML_button.Enabled := true;
+//          set_date_button.Enabled :=true;
+//          ClearMemory_button.Enabled :=true;
+//        end
+//основа
 
-      else
-      begin
-        hilowInt := 0;
-        recievedValue := Str;
-        recievedValue := StringReplace(recievedValue, '<', '', [rfReplaceAll]);
-        recievedValue := StringReplace(recievedValue, '>', '', [rfReplaceAll]);
-        recievedValue := StringReplace(recievedValue, #10, '', [rfReplaceAll]);
-
-        subStrings := SplitString(recievedValue, ' ');
-
-        byteArray[0] := Byte(StrToInt(subStrings[6]));   //старший
-        byteArray[1] := Byte(StrToInt(subStrings[7]));  //младший
-
-        hilowInt:= (byteArray[0] shl 8) or byteArray[1];//совмещаем старший и младший байт
-
-        SetLength(subStrings,7);//сокращаем массив на 1 элемент
-                                                                  //ОСТАНОВИЛСЯ ТУТ, СДЕЛАТЬ АДЕКВАТНОЕ СОЕДИНЕНИЕ
-        subStrings[6] := IntToStr(hilowInt);
-
-        recievedValue := '';
-
-      for J := 0 to 6 do
-        begin
-          Data[I,J] :=StrToInt(SubStrings[J]);
-          recievedValue := recievedValue + IntToStr(Integer(Data[I,J])) +' ';
-        end;;
-
-      I := I+1;
-    end;
-      proc:= ((I*100) div Length(Data));
-      read_progress_bar.Position :=proc;
-      if proc < 100 then
-        begin
-          Connect_button.Enabled := false;
-          read_data_button.Enabled := false;
-          XML_button.Enabled := false;
-          set_date_button.Enabled :=false;
-          ClearMemory_button.Enabled :=false;
-        end
-        else
-        begin
-          Connect_button.Enabled := true;
-          read_data_button.Enabled := true;
-          XML_button.Enabled := true;
-          set_date_button.Enabled :=true;
-          ClearMemory_button.Enabled :=true;
-        end
-
-end;
+//end;
 
 procedure TForm1.XML_buttonClick(Sender: TObject);
 var
